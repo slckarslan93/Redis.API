9f06413ba301f00228e284c13edff570143ee761
Selcuk ARSLAN <slckarslan93@gmail.com>
2023-02-27 09:48:16 +0300

    Repositories

---
+++ Redis.API/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Redis.API.Models;
+
+namespace Redis.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAsync();
+
+        Task<Product> GetByIdAsync(int id);
+
+        Task<Product> CreateAsync(Product product);
+    }
+}
---
+++ Redis.API/Repositories/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using Redis.API.Models;
+
+namespace Redis.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProductRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
